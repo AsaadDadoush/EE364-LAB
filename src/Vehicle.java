83cb6bcee7d8550b99e14f18944c82bc6185b74a
HeshamTB <hishaminv@gmail.com>
2020-11-06 22:42:15 +0300

    Add Route to Vehicle
    	This is debatable since every Campgians has routes
    	but is done to support all cases.
    
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,12 +1,21 @@
 public abstract class Vehicle {
 
     private double vehicleSize;
+    private Route route;
 
     public Vehicle(double vehicleSize){
         setVehicleSize(vehicleSize);
     }
     public double getVehicleSize() {
         return vehicleSize;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
     }
 
     private void setVehicleSize(double vehicleSize) {
