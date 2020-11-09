0bc231222a39bf029f2061f6666699f48fdb5a8f
HeshamTB <hishaminv@gmail.com>
2020-11-09 23:53:42 +0300

    StreetNames:
    	Index values of streets
    
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

---
+++ src/StreetNames.java
@@ -0,0 +1,15 @@
+/**
+ * Street names with their corresponding indices in MakkahCity.
+ */
+public class StreetNames {
+
+    public static final int KA_STREET = 0;
+    public static final int THIRD_HIGHWAY = 1;
+    public static final int FOURTH_HISHWAY = 2;
+    public static final int STREET1 = 3;
+    public static final int STREET2 = 4;
+    public static final int STREET3 = 5;
+    public static final int STREET4 = 6;
+    public static final int STREET5 = 7;
+
+}
