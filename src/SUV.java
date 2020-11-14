3e1b1f162cc447f8521b72623f8bd40a17d671ee
Asaad Dadoush <asaaddaadouch246@gmail.com>
2020-11-14 16:31:43 +0300

    Add UID for Sedan / SUV / Truck

--- src/SUV.java
+++ src/SUV.java
@@ -1,16 +1,29 @@
 
 public class SUV extends CivilVehicle {
-
+	private String UID;
+	private static int numeberOfSUV;
     private final int TIME_TO_FIX = 15; //in minutes
     public static final int MAX_FORWARD = 1300;
 
     public SUV(double vehicleSize){
         super(vehicleSize);
+        generateUID();
     }
 
     public int getTimeToFix(){
     	return TIME_TO_FIX; 
     }
+    
+    private void generateUID() {
+    	numeberOfSUV++;
+    	this.UID = String.format("SUV%04d", numeberOfSUV);
+    	
+    }
+    
+    public String getUID(){
+        return this.UID;
+    }
+    
 
 
 }
